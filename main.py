e1a62cf4f0640c46074ed9eba1761d7c2db6f650
Sanjar <sanjar.ravshanbekov@gmail.com>
2022-10-02 18:33:30 +0500

    Response and response_model

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
 from fastapi import FastAPI, Query, Path, Body
 from fastapi.params import Body
 
-from schemas import Book, Author
+from schemas import Book, BookOut
 from typing import List
 
 app = FastAPI()
 
 
-@app.get('/')
-def home():
-    return {'key': 'hello'}
+#
+# @app.get('/')
+# def home():
+#     return {'key': 'hello'}
+#
+#
+# @app.get('/{pk}')
+# def get_item(pk: int, q: str = None):
+#     return {'key': pk, 'q': q}
+#
+#
+# @app.get('/user/{pk}/items/{item}/')
+# def get_user_item(pk: int, item: str):
+#     return {'user': pk, 'item': item}
+#
+#
+# @app.post('/book', response_model=Book, response_model_include={'date', 'pages'})
+# def create_book(item: Book):
+#     return {'item': item}
+#
+#
+# @app.get('/book/')
+# def get_book(q: List[str] = Query(..., description='Search book')):
+#     return q
+#
+#
+# @app.get('/book/{pk}')
+# def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+#     return {'pk': pk, 'pages': pages}
+#
+#
+# @app.post('/author/')
+# def create_author(author: Author = Body(..., embed=True)):
+#     return {'author': author}
 
-
-@app.get('/{pk}')
-def get_item(pk: int, q: str = None):
-    return {'key': pk, 'q': q}
-
-
-@app.get('/user/{pk}/items/{item}/')
-def get_user_item(pk: int, item: str):
-    return {'user': pk, 'item': item}
-
-
-@app.post('/book/')
-def create_book(item: Book, author: Author, quantity: int = Body(...)):
-    return {'item': item, 'author': author, 'quantity': quantity}
-
-
-@app.get('/book/')
-def get_book(q: List[str] = Query(..., description='Search book')):
-    return q
-
-
-@app.get('/book/{pk}')
-def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
-    return {'pk': pk, 'pages': pages}
-
-
-@app.post('/author/')
-def create_author(author: Author = Body(..., embed=True)):
-    return {'author': author}
+@app.post('/book', response_model=BookOut)
+def create_book(item: Book):
+    book = item.dict()
+    book['id'] = 5
+    return book
+    # return BookOut(**item.dict(), id=3)
--- schemas.py
+++ schemas.py
@@ -1,29 +1,39 @@
 from typing import List
 from datetime import date
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, Field
 
 
-class Genre(BaseModel):
-    name: str
-
-
-class Author(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-
-    @validator('age')
-    def check_age(cls, v):
-        if v < 15:
-            raise ValueError('Возраст автора не может быть меньше 15')
-        return v
-
-
+#
+#
+# class Genre(BaseModel):
+#     name: str
+#
+#
+# class Author(BaseModel):
+#     # first_name: str = Field(..., max_length=25)
+#     # last_name: str
+#     # age: int = Field(..., gt=15, le=90, description='Возраст автора не может быть меньше 15 и больше 90')
+#
+#     first_name: str
+#     last_name: str
+#     age: int
+#
+#     @validator('age')
+#     def check_age(cls, v):
+#         if v < 15:
+#             raise ValueError('Возраст автора не может быть меньше 15')
+#         return v
+#
+#
 class Book(BaseModel):
     title: str
     writer: str
     duration: str
     date: date
     summary: str
-    genres: List[Genre]
-    pages: int
+    # genres: List[Genre] = []
+    pages: int = 10
+
+
+class BookOut(Book):
+    id: int
